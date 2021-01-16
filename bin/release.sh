9e3c7e36c8270394a6b523e5f034e29ebc44eb36
vagrant <vagrant@vagrant.vm>
2021-01-16 18:13:40 +0000

    BESman utility changes by senthil

--- bin/release.sh
+++ bin/release.sh
@@ -19,7 +19,8 @@ fi
 
 if [[ -z $BESMAN_NAMESPACE ]];
     then
-        BESMAN_NAMESPACE={BESMAN_NAMESPACE:-hyperledgerkochi}
+        #BESMAN_NAMESPACE={BESMAN_NAMESPACE:-hyperledgerkochi}
+	BESMAN_NAMESPACE={BESMAN_NAMESPACE:-senthilbk}
 fi
 
 # prepare branch
