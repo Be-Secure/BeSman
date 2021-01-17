2a8d635060a1839b90c17027da684ae293666f0f
vagrant <vagrant@vagrant.vm>
2021-01-17 12:23:11 +0000

    BESman utility changes by senthil

--- bin/dopublish.sh
+++ bin/dopublish.sh
@@ -19,8 +19,8 @@ mkdir -p build/tmp
 
 # making of zip files
 
-#zip -rj $HOME/BESman/build/tmp/besman-latest.zip $HOME/BESman/dist/list.txt $HOME/BESman/src/main/bash/besman-* $HOME/BESman/src/main/bash/envs/besman-* $HOME/BESman/src/main/bash/commands/besman-*
-zip -rj $HOME/BeSman/build/tmp/besman-latest.zip $HOME/BeSman/dist/list.txt $HOME/BeSman/src/main/bash/besman-* $HOME/BESman/src/main/bash/envs/besman-* $HOME/BESman/src/main/bash/commands/besman-*
+zip -rj $HOME/BESman/build/tmp/besman-latest.zip $HOME/BESman/dist/list.txt $HOME/BESman/src/main/bash/besman-* $HOME/BESman/src/main/bash/envs/besman-* $HOME/BESman/src/main/bash/commands/besman-*
+#zip -rj $HOME/BeSman/build/tmp/besman-latest.zip $HOME/BeSman/dist/list.txt $HOME/BeSman/src/main/bash/besman-* $HOME/BESman/src/main/bash/envs/besman-* $HOME/BESman/src/main/bash/commands/besman-*
 
 #zip -r build/tmp/besman-latest.zip $HOME/BESman/src/
 cp $HOME/BESman/build/tmp/besman-latest.zip $HOME/BESman/build/tmp/besman-$bes_rel_version.zip
@@ -45,4 +45,5 @@ git commit -m "Released the version $bes_rel_version"
 git push origin -f -u $dist_branch
 
 #checkout back to master
-git checkout master
+#git checkout master
+git checkout dev
