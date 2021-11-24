ae0287c6a165173d077828d9a17ae172558c14d1
asa1997 <arun.suresh@wipro.com>
2021-11-24 16:21:43 +0530

    checks added for CVE & assessment types

--- src/main/bash/besman-env-helpers.sh
+++ src/main/bash/besman-env-helpers.sh
@@ -191,3 +191,20 @@ function __besman_error_rollback
   fi
 
 }
+
+function __besman_cve_format_check
+{
+  local cve=$1
+  echo "$cve" | grep -qwE "CVE-[0-9]{4}-[0-9]{4,}"
+  [[ "$?" != "0" ]] && __besman_echo_red "CVE format incorrect"  && __besman_echo_no_colour "Format: CVE-YYYY-NNNN..." && return 1
+  unset cve
+}
+
+function __besman_validate_assessment
+{
+  local type=$1
+  assessments=("active" "passive" "external" "internal" "host" "network" "application" "db" "wireless" "distributed" "credentialed" "non-credentialed")
+  echo "${assessments[@]}" | grep -qw "$type"
+  [[ "$?" != "0" ]] && __besman_echo_red "Could not find assessment type" &&  __besman_echo_no_colour "Select from the following:" && echo "${assessments[@]}" && return 1
+  unset type assessments
+}
