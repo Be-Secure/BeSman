528a09b028876fef0be07b0f243ce6140cfdfc7a
asa1997 <arun.suresh@wipro.com>
2022-01-10 16:26:58 +0530

    Added code for listing playbooks, fixed issue #30

--- src/main/bash/commands/besman-list.sh
+++ src/main/bash/commands/besman-list.sh
@@ -2,10 +2,18 @@
 
 function __bes_list {
 
+if [[ -n $1 ]]; then
+    ls $BESMAN_DIR/playbook >> $HOME/temp.txt
+    __besman_echo_no_colour "Available playbooks"
+    __besman_echo_no_colour "-------------------"
+    cat $HOME/temp.txt | grep -v "README.md"
+    [[ -f $HOME/temp.txt ]] && rm $HOME/temp.txt
+else
 sed -i '/^$/d' $BESMAN_DIR/var/list.txt
-
 __besman_echo_no_colour "Available environments and their respective version numbers"
 __besman_echo_no_colour "---------------------------------------------------------------"
 sed 's/,/ - /1' $BESMAN_DIR/var/list.txt
 __besman_echo_no_colour ""
+# unset contents list_file 
+fi
 }
