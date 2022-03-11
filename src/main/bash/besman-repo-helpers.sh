831523b993696bdbd3f1e39603f92800fe83ad42
jobyko <joby.ouseph@wipro.com>
2022-03-11 17:20:38 +0530

    [update] BeSman updated with 'bes publish' command

--- src/main/bash/besman-repo-helpers.sh
+++ src/main/bash/besman-repo-helpers.sh
@@ -65,6 +65,23 @@ function __besman_gh_fork
     gh repo fork $namespace/$repo --clone=false
 }
 
+
+function __besman_gh_issue-pr
+{
+    local filename=$1
+    nano ./.templates/issue_template
+    issue_id=$(gh issue create -t "[Issue for playbook $filename]"  -F "./.templates/issue_template" -R "Be-Secure/$BESMAN_PLAYBOOK_REPO" | sed 's|.*/||' )
+    if [[ $? -eq 0 ]]; then
+        nano ./.templates/pr_template
+        gh pr  create -t "[Publish] Playbook name: $filename | issue id : $issue_id"  -F "./.templates/pr_template" -R "Be-Secure/$BESMAN_PLAYBOOK_REPO"
+
+    fi
+
+
+    unset filename
+}
+
+
 function __besman_check_github_id
 {
 
