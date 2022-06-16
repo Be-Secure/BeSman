f55b5566c844c1931460fdf41b12ce5860f54f9f
asa1997 <arunsureshampadath@gmail.com>
2022-06-16 16:13:41 +0530

    Added function to update req, sanity check; renamed functions

--- src/main/bash/besman-ansible-helpers.sh
+++ src/main/bash/besman-ansible-helpers.sh
@@ -1,19 +1,79 @@
 #!/bin/bash
 
-function __besman_ansible_playbook
+
+function __besman_check_if_ansible_env_vars_exists
 {
-    __besman_echo_white "Running $1"
-    ansible-playbook $1 --ask-become-pass
+    if [[ -z $BESMAN_ANSIBLE_ROLE_PATH ]]; then
+
+        cat <<EOF
+Please use the below command to set the 
+download path for ansible roles
+
+    $ export BESMAN_ANSIBLE_ROLE_PATH=$BESMAN_USER_HOME/<some_path>
+
+EOF
+        return 1
+    fi
+
+    if [[ -z $BESMAN_ANSIBLE_GALAXY_ROLES ]]; then
+
+        cat <<EOF
+
+Please use the below command to add the
+roles you wish to install
+
+    $ export BESMAN_ANSIBLE_GALAXY_ROLES=<namespace>/<repo_name>:<namespace>/<repo_name>:<namespace>/<repo_name>:....
+
+EOF
+        return 1
+    fi    
 }
 
-function __besman_ansible_playbook_extra_vars
+
+
+function __besman_update_requirements_file
 {
-    __besman_echo_white "Running $1 with --extra-vars $2"
-    ansible-playbook $1 --ask-become-pass --extra-vars "$2"
+    local roles namespace repo_name github_url
+
+    github_url=https://github.com
+    roles=$(echo $BESMAN_ANSIBLE_GALAXY_ROLES | sed 's/:/ /g')
+    [[ ! -f $BESMAN_ANSIBLE_ROLE_PATH/requirements.yml ]] && touch $BESMAN_ANSIBLE_ROLE_PATH/requirements.yml && echo "---" >> $BESMAN_ANSIBLE_ROLE_PATH/requirements.yml 
+    for i in ${roles[@]}; do
+        namespace=$(echo $i | cut -d "/" -f 1)
+        repo_name=$(echo $i | cut -d "/" -f 2)
+        if ! cat $BESMAN_ANSIBLE_ROLE_PATH/requirements.yml | grep -wq "$github_url/$namespace/$repo_name"
+        then
+            echo "- src: $github_url/$namespace/$repo_name" >> $BESMAN_ANSIBLE_ROLE_PATH/requirements.yml
+            continue
+        else
+            __besman_echo_no_colour "Ignoring role $github_url/$namespace/$repo_name as it is already present in requirements.yml"
+        fi
+    done
+    unset roles namespace repo_name github_url
 }
 
-function __besman_ansible_galaxy_install_from_requirements
+function __besman_run_ansible_playbook_with_become
 {
-    __besman_echo_white "Installing ansible roles from $1/requirements.yml under $2"
-    ansible-galaxy install -r $1/requirements.yml -p $2
+    local playbook=$1
+    __besman_echo_white "Running $playbook"
+    ansible-playbook $playbook --ask-become-pass
+    unset playbook
+}
+
+function __besman_run_ansible_playbook_extra_vars
+{
+    local playbook vars
+    playbook=$1
+    vars=$2
+    __besman_echo_white "Running $playbook with --extra-vars $vars"
+    ansible-playbook $playbook --ask-become-pass --extra-vars "$vars"
+    unset playbook vars
+}
+
+
+
+function __besman_ansible_galaxy_install_roles_from_requirements
+{
+    __besman_echo_white "Installing ansible roles from $BESMAN_ANSIBLE_ROLE_PATH/requirements.yml under $BESMAN_ANSIBLE_ROLE_PATH"
+    ansible-galaxy install -r $BESMAN_ANSIBLE_ROLE_PATH/requirements.yml -p $BESMAN_ANSIBLE_ROLE_PATH
 }
