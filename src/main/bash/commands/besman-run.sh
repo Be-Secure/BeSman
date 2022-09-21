7d3ba4634bc4aa3a58fb45ba0e59a57bf2891f42
asa1997 <arunsureshampadath@gmail.com>
2022-09-21 14:49:45 +0530

    Updated command to include inputs

--- src/main/bash/commands/besman-run.sh
+++ src/main/bash/commands/besman-run.sh
@@ -2,22 +2,41 @@
 
 function __bes_run
 {
-    local playbook
-    playbook=$1
-
+    local playbook result inputs
+    playbook=$2
+    inputs=$( echo $@ | cut -d " " -f 3-)
     __besman_get_playbook_extension "$playbook"
     if [[ "$?" -eq 0 ]]; then
         __besman_echo_white "Running playbook as shell script"
-        ./$BESMAN_PLAYBOOK_DIR/$playbook.sh
+        source $BESMAN_PLAYBOOK_DIR/$playbook.sh "${inputs[@]}"
+        result=$?
+        __besman_output_result "$result"
     elif [[ "$?" -eq 1 ]]; then
         __besman_echo_white "Opening playbook as md file"
         code $BESMAN_PLAYBOOK_DIR/$playbook.md
+        result=$?
+        __besman_output_result "$result"
     elif [[ "$?" -eq 2 ]]; then
         __besman_echo_white "Running playbook as yml file"
         ansible-playbook --ask-become-pass $BESMAN_PLAYBOOK_DIR/$playbook.yml
+        result=$?
+        __besman_output_result "$result"
     elif [[ "$?" -eq 3 ]]; then
         __besman_echo_red "Besman only supports playbooks with md/sh/yml extensions"
         return 1
+    fi
+
+
+}
+
+function __besman_output_result
+{
+    local result=$1
+
+    if [[ $result -eq 0 ]]; then
+        __besman_echo_green "Done."
+    elif [[ $result -eq 1 ]]; then
+        __besman_echo_red "Something went wrong"
     fi
 
 
