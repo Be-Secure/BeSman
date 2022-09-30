b4d27849468722d7f893e855a12299ad7d4bab3f
asa1997 <arunsureshampadath@gmail.com>
2022-09-30 10:00:31 +0530

    Fixed #54 #55

--- src/main/bash/commands/besman-list.sh
+++ src/main/bash/commands/besman-list.sh
@@ -2,18 +2,23 @@
 
 function __bes_list {
 
-if [[ -n $1 ]]; then
+local playbook_flag=$1
+if [[ ( -n $playbook_flag ) && ( -d $BESMAN_DIR/playbook ) ]]; then
+    [[ -z $(ls $BESMAN_DIR/playbook) ]] && __besman_echo_white "No playbook available" && return 1
     ls $BESMAN_DIR/playbook >> $HOME/temp.txt
     __besman_echo_no_colour "Available playbooks"
     __besman_echo_no_colour "-------------------"
     cat $HOME/temp.txt | grep -v "README.md"
     [[ -f $HOME/temp.txt ]] && rm $HOME/temp.txt
+elif [[ ( -n $playbook_flag ) && ( ! -d $BESMAN_DIR/playbook ) ]]; then
+    __besman_echo_white "No playbook available"
+
 else
 sed -i '/^$/d' $BESMAN_DIR/var/list.txt
 __besman_echo_no_colour "Available environments and their respective version numbers"
 __besman_echo_no_colour "---------------------------------------------------------------"
 sed 's/,/ - /1' $BESMAN_DIR/var/list.txt
 __besman_echo_no_colour ""
-# unset contents list_file 
+unset playbook_flag
 fi
 }
