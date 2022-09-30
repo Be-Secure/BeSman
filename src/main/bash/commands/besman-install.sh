d5715f4ec04dee6a6852fdc4bbe50acc0bebd6b6
asa1997 <arunsureshampadath@gmail.com>
2022-09-30 09:35:40 +0530

    Fixed #52

--- src/main/bash/commands/besman-install.sh
+++ src/main/bash/commands/besman-install.sh
@@ -4,6 +4,7 @@
 function __bes_install {
 	local environment_name=$1
 	local version_id=$2
+	local return_val
 	mkdir -p ${BESMAN_DIR}/envs/besman-"${environment_name}"
 	touch ${BESMAN_DIR}/envs/besman-${environment_name}/current
 	current="${BESMAN_DIR}/envs/besman-${environment_name}/current"
@@ -20,14 +21,35 @@ function __bes_install {
 		cp "${BESMAN_DIR}/envs/besman-${environment_name}.sh" ${BESMAN_DIR}/envs/besman-${environment_name}/$version_id/
 		source "${BESMAN_DIR}/envs/besman-${environment_name}/${version_id}/besman-${environment_name}.sh"
 		__besman_install_"${environment_name}" "${environment_name}" "${version_id}"
+		return_val="$?"
+		__besman_manage_install_out "$return_val" "$environment_name"
+
 	elif [[ -d ${BESMAN_DIR}/envs/besman-${environment_name}/$version_id && $(cat ${BESMAN_DIR}/envs/besman-${environment_name}/current) != "$version_id" ]];
 	then
 		__besman_echo_no_colour "Re-installing ${environment_name} with version:${version_id} "
 		__besman_echo_no_colour "$version_id" > "$current"
 		__besman_install_"${environment_name}" "${environment_name}" "${version_id}"
+		return_val="$?"
+		__besman_manage_install_out "$return_val" "$environment_name"
+
 	else
 		__besman_echo_white "${environment_name} $version_id is currently installed in your system "
 		
+	fi
+
+}
+
+function __besman_manage_install_out
+{
+	local return_val environment
+	return_val=$1
+	environment=$2
+
+	if [[ $return_val == "0" ]]; then
+		__besman_echo_green "Successfully installed $environment"
+	else
+		__besman_echo_red "Installation failed"
+		__besman_error_rollback "$environment"
 	fi
 
 }
