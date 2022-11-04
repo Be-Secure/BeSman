5752fdf898a1249c1284b42c3e9552df4a4baaa4
asa1997 <arunsureshampadath@gmail.com>
2022-11-04 16:32:11 +0530

    Fixed #66

--- src/main/bash/commands/besman-list.sh
+++ src/main/bash/commands/besman-list.sh
@@ -3,6 +3,7 @@
 function __bes_list {
 
 local playbook_flag=$1
+local env
 if [[ ( -n $playbook_flag ) && ( -d $BESMAN_DIR/playbook ) ]]; then
     [[ -z $(ls $BESMAN_DIR/playbook | grep -v "README.md") ]] && __besman_echo_white "No playbook available" && return 1
     ls $BESMAN_DIR/playbook >> $HOME/temp.txt
@@ -14,11 +15,31 @@ elif [[ ( -n $playbook_flag ) && ( ! -d $BESMAN_DIR/playbook ) ]]; then
     __besman_echo_white "No playbook available"
 
 else
-sed -i '/^$/d' $BESMAN_DIR/var/list.txt
-__besman_echo_no_colour "Available environments and their respective version numbers"
-__besman_echo_no_colour "---------------------------------------------------------------"
-sed 's/,/ - /1' $BESMAN_DIR/var/list.txt
-__besman_echo_no_colour ""
-unset playbook_flag
+    __besman_echo_no_colour "Available environments and their respective version numbers"
+    __besman_echo_no_colour "---------------------------------------------------------------"
+    
+    sed -i '/^$/d' $BESMAN_DIR/var/list.txt
+      
+    while read line; 
+    do 
+
+        env=$(echo $line | cut -d "," -f 1 | cut -d "/" -f 3)
+        if [[ -f $BESMAN_DIR/envs/besman-$env.sh ]]; then
+
+            echo "$line (local)" | sed 's/,/ - /1'
+        
+        else
+
+            echo "$line (remote)" | sed 's/,/ - /1'
+
+        fi
+        
+    done < $BESMAN_DIR/var/list.txt
+
+
+    __besman_echo_no_colour ""
+
+    unset playbook_flag
+
 fi
 }
