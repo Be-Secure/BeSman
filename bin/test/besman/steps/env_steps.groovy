4d5cf54955032f1637112a1fe90b1bc3517c44b7
asa1997 <arunsureshampadath@gmail.com>
2022-11-04 17:33:49 +0530

    Revert "Removed BeSman dev env"
    This reverts commit 0bd73883c33bf008fd443bbafa584ee9295e1ea8.

--- bin/test/besman/steps/env_steps.groovy
+++
@@ -1,9 +0,0 @@
-package besman.steps
-
-import static cucumber.api.groovy.EN.And
-
-And(~/^the file "([^"]+)" exists and contains "([^"]+)"$/) { String filename, String content ->
-	new File(besmanBaseEnv, filename).withWriter {
-		it.writeLine(content)
-	}
-}
