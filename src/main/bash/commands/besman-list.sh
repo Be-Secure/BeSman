1f72ebdac80dda046c2ba42852a317f5df60b71f
asa1997 <arunsureshampadath@gmail.com>
2022-11-07 13:55:43 +0530

    List shows local & remote versions of env

--- src/main/bash/commands/besman-list.sh
+++ src/main/bash/commands/besman-list.sh
@@ -4,6 +4,8 @@ function __bes_list {
 
 local playbook_flag=$1
 local env
+
+# For listing playbooks
 if [[ ( -n $playbook_flag ) && ( -d $BESMAN_DIR/playbook ) ]]; then
     [[ -z $(ls $BESMAN_DIR/playbook | grep -v "README.md") ]] && __besman_echo_white "No playbook available" && return 1
     ls $BESMAN_DIR/playbook >> $HOME/temp.txt
@@ -15,31 +17,59 @@ elif [[ ( -n $playbook_flag ) && ( ! -d $BESMAN_DIR/playbook ) ]]; then
     __besman_echo_white "No playbook available"
 
 else
-    __besman_echo_no_colour "Available environments and their respective version numbers"
-    __besman_echo_no_colour "---------------------------------------------------------------"
+    # For listing environments
+    __besman_echo_no_colour "Available environments and their respective version numbers (* - local; ^ - remote)"
+    __besman_echo_no_colour "-----------------------------------------------------------------------------------"
     
     sed -i '/^$/d' $BESMAN_DIR/var/list.txt
       
     while read line; 
     do 
 
-        env=$(echo $line | cut -d "," -f 1 | cut -d "/" -f 3)
-        if [[ -f $BESMAN_DIR/envs/besman-$env.sh ]]; then
-
-            echo "$line (local)" | sed 's/,/ - /1'
+        local arr=()
+        arr=$(echo $line | sed 's/,/ /g')
         
-        else
+        local list=""
+        
+        for i in ${arr[@]};
+        do
+        
+            if echo $i | grep -q "env" # If i is env name.
+            then
+        
+                list=$i
+        
+                local env=$(echo $i | cut -d "/" -f 3) # To get the name of the env. Removes namespace and repo name
+        
+            elif [[ -d $BESMAN_DIR/envs/besman-$env ]];  then
 
-            echo "$line (remote)" | sed 's/,/ - /1'
+                if ls $BESMAN_DIR/envs/besman-$env | grep -q "$i" # Checks if version is listed under installed dir of env
+                then
+        
+                    list="$list $i*" # * depicts local
+        
+                else
+        
+                    list="$list $i^" # ^ depicts remote
+        
+                fi
+        
+            else
+        
+                list="$list $i^" # Remote and env not installed
+        
+            fi
 
-        fi
+        done
+        
+        echo $list
         
     done < $BESMAN_DIR/var/list.txt
 
 
     __besman_echo_no_colour ""
 
-    unset playbook_flag
+    unset playbook_flag arr env list
 
 fi
 }
