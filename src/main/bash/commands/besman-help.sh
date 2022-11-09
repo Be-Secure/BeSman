95de2edc285d2f54f3a58e912dca36ce90a7727f
asa1997 <arunsureshampadath@gmail.com>
2022-11-09 13:25:06 +0530

    Fixed #68

--- src/main/bash/commands/besman-help.sh
+++ src/main/bash/commands/besman-help.sh
@@ -5,28 +5,57 @@ cat << EOF
 BeSman - Help 
 ---------------------------------------------------------
 $ bes <command> <qualifer> 
+
 <qualifier>   : environment, version, namespace
+
 <command> 
  ----------------------------------------------------------
-install       : $ bes install –env [env_name] -V [version]
+install       : $ bes install –env [namespace]/[repo name]/[environment name] -V [version]
 		The command is used to install the specified
 		environment.
-		Eg: $ bes install -env BeSman –V 0.0.2
+
+		Eg: $ bes install -env Be-Secure/besecure-ce-env-repo/BeSman-dev-env –V 0.0.1
+
 uninstall     : $ bes uninstall --environment [env_name]
-		or
-		$ bes uninstall –env [env_name] -V [version]
+
+						OR
+
+			$ bes uninstall –env [env_name] -V [version]
+		
 		The command is used to uninstall the specified
 		environment.
+		
 		Eg: $ bes uninstall –env BeSman
+
 status        : $ bes status
+
 		The command displays the installed environments.
+
 list          : $ bes list
+
 		The command lists the various environment that
 		can be installed.
+				
+				OR
+			  
+		  : $ bes list --playbook
+		
+		Used to list the playbooks
+
 upgrade       : $ bes upgrade
+
 		Upgrades BeSman to next version available.
+
 update        : $ bes update
+
 		Updates the list file with lastest changes.
+				
+				OR
+			  
+		  : $ bes update -env [environment name]
+		
+		Update the environment.
+		
 version       : Version of BeSman Utility
 		-------------------------
 		$ bes --version or bes –V
@@ -38,9 +67,13 @@ version       : Version of BeSman Utility
 		The command displays the version of the specified
 		environment.
 		Eg: $ bes -V -env greenlight
+
 help          : $ bes help
+
 		Displays the BeSman manual
+
 remove 	      : $ bes rm
+
 		Removes BeSman utility and installed environments 
 		from the local system
 
@@ -60,7 +93,7 @@ create        : To create a playbook. The playbook will be named
 			-ext : To pass the extension of the playbook.
 					$ bes create --playbook -ext py
 
-			NOTE: If any of the above values are not passed. Default value "untitled" will be assigned in its place except for -ext. 
+			NOTE: If any of the above values are not passed, default value "untitled" will be assigned in its place except for -ext. 
 					If -ext is not passed, the default value will be "md".
 		
 		Eg: $ bes create --playbook -cve CVE-2018-2019 -vuln rce -env drupal -ext php
@@ -70,6 +103,9 @@ pull          : $ bes pull --playbook
 
 		To pull down playbooks from Be-Secure playbook repository.
 
+run 		  : $ bes run --playbook [playbook name] --input [input arguments]
+		
+		To run the playbook
 
 EOF
 }
