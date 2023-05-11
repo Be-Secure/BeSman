1eee0ef8c14e65ed677a7e6a7d986a0bf171d415
asa1997 <arunsureshampadath@gmail.com>
2023-05-11 15:00:46 +0530

    Added code to create env files

--- src/main/bash/commands/besman-create.sh
+++ src/main/bash/commands/besman-create.sh
@@ -13,7 +13,7 @@ function __bes_create
         # checks whether the user github id has been populated or not under $BESMAN_USER_NAMESPACE 
         __besman_check_github_id || return 1
         # checks whether the user has already logged in or not to gh tool
-        __besman_gh_auth_status $BESMAN_USER_NAMESPACE
+        __besman_gh_auth_status "$BESMAN_USER_NAMESPACE"
         return_val=$?
         # if return_val == 0 then the user is already logged in
         if [[ $return_val == "0" ]]; then
@@ -29,13 +29,13 @@ function __bes_create
         fi
         
         __besman_echo_white "forking"
-        __besman_gh_fork $BESMAN_NAMESPACE $BESMAN_PLAYBOOK_REPO 
+        __besman_gh_fork "$BESMAN_NAMESPACE" "$BESMAN_PLAYBOOK_REPO" 
         
         [[ "$?" != "0" ]] && return 1        
         
         if [[ ! -d $HOME/$BESMAN_PLAYBOOK_REPO ]]; then
             __besman_echo_white "cloning"  
-            __besman_gh_clone $BESMAN_USER_NAMESPACE $BESMAN_PLAYBOOK_REPO $HOME/$BESMAN_PLAYBOOK_REPO
+            __besman_gh_clone "$BESMAN_USER_NAMESPACE" "$BESMAN_PLAYBOOK_REPO" "$HOME/$BESMAN_PLAYBOOK_REPO"
             [[ "$?" != "0" ]] && return 1
         
         fi
@@ -54,11 +54,60 @@ function __bes_create
 
         unset vuln env ext target_path return_val purpose
     else
-    # TODO
-        create_env
-    
+        # bes create -env fastjson-RT-env 
+
+        local environment_name
+        environment_name=$2
+        __besman_create_env "$environment_name" || return 1
+        __besman_update_list "$environment_name"     
     fi
 }
+
+function __besman_create_env
+{
+    export LOCAL_ENV=True
+    local environment_name=$1
+    local env_file=$BESMAN_DIR/envs/besman-$environment_name.sh
+    [[ -f $env_file ]] && __besman_echo_red "Environment file exists" && return 1
+    cat <<EOF >> "$env_file"
+#!/bin/bash
+
+function __besman_install_$environment_name
+{
+
+}
+
+function __besman_uninstall_$environment_name
+{
+    
+}
+
+function __besman_update_$environment_name
+{
+    
+}
+
+function __besman_validate_$environment_name
+{
+    
+}
+
+function __besman_reset_$environment_name
+{
+    
+}
+EOF
+__besman_echo_white "Creating env file.."
+code "$env_file"
+}
+
+function __besman_update_list()
+{
+    local environment_name=$1
+    echo "Local/Local/$environment_name,0.0.1" >> $BESMAN_DIR/var/list.txt
+    
+}
+
 
 function __besman_create_playbook
 {
@@ -98,8 +147,4 @@ function __besman_create_playbook
     unset args vuln env ext purpose
 }   
 
-# function create_env
-# {
-#     # TODO
-# }
 
