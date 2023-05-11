584194eeccbdadebfedf05d172b2e871ddfe29f3
asa1997 <arunsureshampadath@gmail.com>
2023-05-11 15:01:07 +0530

    Updated code to test envs locally

--- src/main/bash/commands/besman-install.sh
+++ src/main/bash/commands/besman-install.sh
@@ -1,53 +1,55 @@
 #!/usr/bin/env bash
 
-
 function __bes_install {
-	local input_environment_name=$1 # The format of the input environment name: <env_namespace>/<env_repo_name>/<env_name>
-	
-	# Syntax check for var input_environment_name. 
-	# Format of input environment name = <namespace>/<repo name>/<environment name>
-	# Word count of var input_environment_name, after replace '/' with ' ', will be 3
-	[[ $(echo $input_environment_name | sed  "s/\// /g" | wc -w) -ne 3 ]] && __besman_echo_red "Please provide the environment name as <namespace>/<repo_name>/<env_name>" && return 1
-	
+	local input_environment_name=$1
+	local namespace env_repo environment_name
+	# If not test env and only env name is passed
+	if [[ ($(echo "$input_environment_name" | sed "s/\// /g" | wc -w) -eq 1) && ("$LOCAL_ENV" == "False") ]]; then
+		echo "Defaulting to Be-Secure/besecure-ce-env-repo"
+		namespace="$BESMAN_NAMESPACE"
+		env_repo="besecure-ce-env-repo"
+		environment_name="$input_environment_name"
+	# If not test env and namespace/repo/env_name is passed
+	elif [[ ($(echo "$input_environment_name" | sed "s/\// /g" | wc -w) -eq 3) && ("$LOCAL_ENV" == "False") ]]; then
+		OLD_IFS="$IFS"
+		IFS="/"
+		read -r namespace env_repo environment_name <<<"$input_environment_name"
+		# namespace=$(echo "$input_environment_name" | cut -d "/" -f 1)
+		# repo_name=$(echo "$input_environment_name" | cut -d "/" -f 2)
+		IFS="$OLD_IFS"
+	# If test env
+	elif [[ "$LOCAL_ENV" == "True" ]]; then
+		environment_name="$input_environment_name"
+	fi
+
 	local version_id=$2
 	local return_val ossp env_repo_namespace env_repo environment_name
-	
-	# To get the actual environment name, after removing <namespace> and <repo name>
-	environment_name=$(echo "$input_environment_name" | cut -d "/" -f 3) 
-	
+	#shellcheck=${BESMAN_DIR}/envs/besman-${environment_name}/${version_id}/besman-${environment_name}.sh
 	# If environmnet not installed.
-	if [[ ! -d "${BESMAN_DIR}/envs/besman-${environment_name}/$version_id" ]];
-	then		
+	if [[ ! -d "${BESMAN_DIR}/envs/besman-${environment_name}/$version_id" ]]; then
 
+		# If not test env, fetch the environment from environment repo.
+		[[ $LOCAL_ENV == "False" ]] && __besman_get_remote_env "$namespace" "$env_repo" "$environment_name"
 
-		__besman_get_remote_env "$input_environment_name" "$environment_name" || return 1
-
-		mkdir -p ${BESMAN_DIR}/envs/besman-"${environment_name}"
-		touch ${BESMAN_DIR}/envs/besman-${environment_name}/current
+		mkdir -p "${BESMAN_DIR}/envs/besman-${environment_name}"
+		touch "${BESMAN_DIR}/envs/besman-${environment_name}/current"
 		current="${BESMAN_DIR}/envs/besman-${environment_name}/current"
-		
-		
-		mkdir -p ${BESMAN_DIR}/envs/besman-${environment_name}/$version_id
 
-		__besman_echo_no_colour "$version_id" > "$current"
+		mkdir -p "${BESMAN_DIR}/envs/besman-${environment_name}/$version_id"
 
-		if [[ -f $HOME/besman-$environment_name.yml ]]; then
-			__besman_echo_white "Sourcing env parameters"
-			__besman_source_env_parameters "$environment_name"
-		fi
+		__besman_echo_no_colour "$version_id" >"$current"
 
-		cp "${BESMAN_DIR}/envs/besman-${environment_name}.sh" ${BESMAN_DIR}/envs/besman-${environment_name}/$version_id/
+		cp "${BESMAN_DIR}/envs/besman-${environment_name}.sh" "${BESMAN_DIR}/envs/besman-${environment_name}/$version_id/"
 		source "${BESMAN_DIR}/envs/besman-${environment_name}/${version_id}/besman-${environment_name}.sh"
 
 		__besman_install_"${environment_name}" "${environment_name}" "${version_id}"
-		__besman_unset_env_parameters
+
 		return_val="$?"
 
 		__besman_manage_install_out "$return_val" "$environment_name"
 
 	# if environmnet installed, but user wants to install a different version of the same environment.
-	elif [[ -d ${BESMAN_DIR}/envs/besman-${environment_name}/$version_id && $(cat ${BESMAN_DIR}/envs/besman-${environment_name}/current) != "$version_id" ]];
-	then
+	elif [[ -d ${BESMAN_DIR}/envs/besman-${environment_name}/$version_id && $(cat "${BESMAN_DIR}/envs/besman-${environment_name}/current") != "$version_id" ]]; then
 
 		__besman_echo_white "Please remove the existing installation for $environment_name with version $version_id and try again."
 		return 1
@@ -55,70 +57,44 @@ function __bes_install {
 	else
 		# If user tries to install the already installed version of the environment
 		__besman_echo_white "${environment_name} $version_id is currently installed in your system "
-		
+
 	fi
 
 }
 
-function __besman_manage_install_out
-{
+function __besman_manage_install_out {
 	local return_val environment
-	
+
 	return_val=$1
 	environment=$2
 
 	if [[ $return_val == "0" ]]; then
 
 		__besman_echo_green "Successfully installed $environment"
-	
+
 	else
-	
+
 		__besman_echo_red "Installation failed"
 		__besman_error_rollback "$environment"
-	
+
 	fi
 
 }
 
-function __besman_get_remote_env
-{
+function __besman_get_remote_env {
 
+	# This code fetches the environment from env repo
 	local input_environment_name environment_name env_repo_namespace env_repo ossp_dir env_url
-	
-	input_environment_name=$1
-	environment_name=$2
-	env_repo_namespace=$(echo "$input_environment_name" | cut -d "/" -f 1)
-	env_repo=$(echo "$input_environment_name" | cut -d "/" -f 2)
-	ossp_dir=$(echo "$input_environment_name" | cut -d "/" -f 3 | cut -d "-" -f 1)
+	env_repo_namespace=$1
+	env_repo=$2
+	environment_name=$3
+	ossp_dir=$(echo "$environment_name" | cut -d "-" -f 1)
 	env_url="https://raw.githubusercontent.com/${env_repo_namespace}/${env_repo}/master/${ossp_dir}/${version_id}/besman-${environment_name}.sh"
-	
-	__besman_secure_curl "$env_url" >> ${BESMAN_DIR}/envs/besman-${environment_name}.sh
+
+	__besman_secure_curl "$env_url" >>"${BESMAN_DIR}/envs/besman-${environment_name}.sh"
+
 	[[ "$?" -ne 0 ]] && __besman_echo_red "Failed while trying to get the besman-${environment_name}.sh" && return 1
-	
-	# Checks for user level config file for the env. If not found, download the default config file from remote repo.
-	if [[ ! -f $HOME/besman-$environment_name.yml ]]; then
 
-		env_config_url="https://raw.githubusercontent.com/${env_repo_namespace}/${env_repo}/master/${ossp_dir}/${version_id}/besman-${environment_name}.yml"
-		
-		if ( curl -o/dev/null -sfI "$env_config_url" ); then
-		
-			__besman_echo_white "Using environment level configuration."
-			__besman_secure_curl "$env_config_url" >> $HOME/besman-${environment_name}.yml
-
-		else
-		
-			__besman_echo_yellow "Could not get the env level configuration" 
-		
-		fi
-	
-	else
-
-		__besman_echo_white "User level configuration found"
-	
-	fi
-
-	
 	unset input_environment_name environment_name env_repo_namespace env_repo ossp_dir env_url
- 
-		
+
 }
