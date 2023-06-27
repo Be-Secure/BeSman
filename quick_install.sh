07582120ebc8cc03c5c1eb6b97153caaa35c7a7a
asa1997 <arunsureshampadath@gmail.com>
2023-06-27 16:10:41 +0530

    Added steps for installing ansible and gh

--- quick_install.sh
+++ quick_install.sh
@@ -125,6 +125,24 @@ else
 	fi
 fi
 
+if [[ -z $(which ansible) ]]; then
+  echo "Installing ansible"
+  sudo apt-add-repository ppa:ansible/ansible
+  sudo apt update
+  sudo apt install ansible
+fi
+
+if [[ -z $(which gh) ]]; then
+  echo "Installing GitHub Cli"
+  type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg 
+  sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null 
+  sudo apt update
+  sudo apt install gh -y
+
+fi
+
 
 echo "Installing BeSMAN scripts..."
 
