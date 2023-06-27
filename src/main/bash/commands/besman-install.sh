a19ed57e4bf25c81e46e79bbf5478e49b51e950a
asa1997 <arunsureshampadath@gmail.com>
2023-06-27 12:55:12 +0530

    Fix #81

--- src/main/bash/commands/besman-install.sh
+++ src/main/bash/commands/besman-install.sh
@@ -1,34 +1,24 @@
 #!/usr/bin/env bash
 
 function __bes_install {
-	local input_environment_name=$1
-	local namespace env_repo environment_name 
-	if [[ ($(echo "$input_environment_name" | sed "s/\// /g" | wc -w) -eq 1) && ( ! -f $BESMAN_DIR/envs/besman-$input_environment_name.sh ) ]]; then
-		echo "Defaulting to Be-Secure/besecure-ce-env-repo"
-		namespace="$BESMAN_NAMESPACE"
-		env_repo="besecure-ce-env-repo"
-		environment_name="$input_environment_name"
-	elif [[ ($(echo "$input_environment_name" | sed "s/\// /g" | wc -w) -eq 3) && ( ! -f $BESMAN_DIR/envs/besman-$(echo "$input_environment_name" | cut -d "/" -f 3).sh ) ]]; then
-		OLD_IFS="$IFS"
-		IFS="/"
-		read -r namespace env_repo environment_name <<<"$input_environment_name"
-		IFS="$OLD_IFS"
-	elif [[ ($(echo "$input_environment_name" | sed "s/\// /g" | wc -w) -eq 1) && ( -f $BESMAN_DIR/envs/besman-$input_environment_name.sh ) ]]; then
-		__besman_echo_yellow "Installing local environment $input_environment_name"
-		environment_name="$input_environment_name"
+	local environment_name env_repo environment_name version_id
+	environment_name=$1
 
+
+	version_id=$2
+	if [[ ( -n $BESMAN_LOCAL_ENV ) && ( $BESMAN_LOCAL_ENV == "True" ) ]]; then
+		__besman_get_local_env "$environment_name" "$version_id" || return 1
 	fi
-
-	local version_id=$2
-	local return_val ossp env_repo_namespace env_repo environment_name
 	# If environmnet not installed.
 	if [[ ! -d "${BESMAN_DIR}/envs/besman-${environment_name}/$version_id" ]]; then
 
-		[[ ( -n $namespace ) && ( -n $env_repo ) ]] && __besman_get_remote_env "$namespace" "$env_repo" "$environment_name" 
-
+		if [[ ( -n $BESMAN_LOCAL_ENV ) && ( $BESMAN_LOCAL_ENV == "False" ) ]]; then
+			__besman_get_remote_env "$environment_name" || return 1
+		fi
 		mkdir -p "${BESMAN_DIR}/envs/besman-${environment_name}"
 		touch "${BESMAN_DIR}/envs/besman-${environment_name}/current"
 		current="${BESMAN_DIR}/envs/besman-${environment_name}/current"
+		echo "${environment_name}" > "$BESMAN_DIR/var/current"
 
 		mkdir -p "${BESMAN_DIR}/envs/besman-${environment_name}/$version_id"
 
@@ -56,7 +46,34 @@ function __bes_install {
 		__besman_echo_white "${environment_name} $version_id is currently installed in your system "
 
 	fi
+	unset return_val env_repo environment_name namespace version_id
 
+}
+
+function __besman_get_local_env()
+{
+	local environment version default_config_path
+
+	environment=$1
+	version=$2
+	ossp=$(echo "$environment" | cut -d "-" -f 1)
+	default_config_path=$BESMAN_DIR/tmp/besman-$environment_name-config.yaml
+	[[ ! -d $BESMAN_ENV_REPOS ]] && __besman_echo_red "Could not find dir $BESMAN_ENV_REPOS" && return 1
+	cp "$BESMAN_ENV_REPOS/$ossp/$version/besman-$environment.sh" "$BESMAN_DIR/envs/"
+	if [[ ( -n $BESMAN_LIGHT_MODE ) && ( $BESMAN_LIGHT_MODE == "False" ) && ( ! -f "$HOME/besman-$environment-config.yaml" ) ]]; then
+		
+		if [[ -f $default_config_path ]]; then
+			__besman_echo_yellow "A config file already exists"
+			read -rp "Do you wish to replace it(y/n)?: " replace
+		fi
+		if [[ ( -z $replace ) || ( $replace == 'Y' ) || ( $replace == 'y' ) ]]; then
+			rm "$default_config_path"
+			touch "$default_config_path"
+			[[ ! -f "$BESMAN_ENV_REPOS/$ossp/$version/besman-$environment-config.yaml" ]] && __besman_echo_red "Could not find config file in the path $BESMAN_ENV_REPOS/$ossp/$version/" && return 1
+			cp "$BESMAN_ENV_REPOS/$ossp/$version/besman-$environment-config.yaml" "$default_config_path"
+		fi
+	fi
+	
 }
 
 function __besman_manage_install_out {
@@ -81,10 +98,10 @@ function __besman_manage_install_out {
 function __besman_get_remote_env {
 
 	# This code fetches the environment and its config file from env repo
-	local input_environment_name environment_name env_repo_namespace env_repo ossp env_url default_config_path
-	env_repo_namespace=$1
-	env_repo=$2
-	environment_name=$3
+	local environment_name env_repo_namespace env_repo ossp env_url default_config_path replace
+	env_repo_namespace=$(echo "$BESMAN_ENV_REPOS" | cut -d "/" -f 1)
+	env_repo=$(echo "$BESMAN_ENV_REPOS" | cut -d "/" -f 2)
+	environment_name=$1
 	env_type=$(echo "$environment_name" | cut -d "-" -f 2)
 	ossp=$(echo "$environment_name" | cut -d "-" -f 1)
 	env_url="https://raw.githubusercontent.com/${env_repo_namespace}/${env_repo}/master/${ossp}/${version_id}/besman-${environment_name}.sh"
@@ -93,9 +110,16 @@ function __besman_get_remote_env {
 	[[ "$?" -ne 0 ]] && __besman_echo_red "Failed while trying to get the besman-${environment_name}.sh" && return 1
 	if [[ ( -n $BESMAN_LIGHT_MODE ) && ( $BESMAN_LIGHT_MODE == "False" ) && ( ! -f "$HOME/besman-${ossp}-${env_type}-env-config.yaml" ) ]]; then
 		config_url="https://raw.githubusercontent.com/${env_repo_namespace}/${env_repo}/master/${ossp}/${version_id}/besman-${ossp}-${env_type}-env-config.yaml"
-		touch "$default_config_path"
-		__besman_secure_curl "$config_url" >> "$default_config_path"
-		[[ "$?" -ne 0 ]] && __besman_echo_red "Failed while trying to get the besman-${ossp}-${env_type}-env-config.yaml" && return 1
+		if [[ -f $default_config_path ]]; then
+			__besman_echo_yellow "A config file already exists"
+			read -p "Do you wish to replace it(y/n)?: " replace
+		fi
+		if [[ ( -z $replace ) || ( $replace == 'Y' ) || ( $replace == 'y' ) ]]; then
+			rm "$default_config_path"
+			touch "$default_config_path"
+			__besman_secure_curl "$config_url" >> "$default_config_path"
+			[[ "$?" -ne 0 ]] && __besman_echo_red "Failed while trying to get the besman-${ossp}-${env_type}-env-config.yaml" && return 1
+		fi
 	fi
 
 
