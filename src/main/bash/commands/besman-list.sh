60041df8c3a5e4ccdb6820294893f82cd4f870d5
Arun Suresh <58501088+asa1997@users.noreply.github.com>
2023-09-28 14:39:09 +0530

    Fixed Issue#87 (#88)
    * Updated flag
    
    * Fixed #87
    
    * Revert "Fixed #87"
    
    This reverts commit 7f309b7b36bb436c03726af952d91e60835d244d.
    
    * Fixed #87
    
    * Updated function name
    
    * Added gitignore
    
    * Updated code
    
    * Updated

--- src/main/bash/commands/besman-list.sh
+++ src/main/bash/commands/besman-list.sh
@@ -7,18 +7,25 @@ local env sorted_list
 
 # For listing playbooks
 if [[ ( -n $flag ) && ( $flag == "--playbook" ) ]]; then
-    if [[ -d $BESMAN_DIR/playbook  ]]; then
-        [[ -z $(ls $BESMAN_DIR/playbook | grep -v "README.md") ]] && __besman_echo_white "No playbook available" && return 1
-        ls $BESMAN_DIR/playbook >> $HOME/temp.txt
-        __besman_echo_no_colour "Available playbooks"
-        __besman_echo_no_colour "-------------------"
-        cat $HOME/temp.txt | grep -v "README.md"
-        [[ -f $HOME/temp.txt ]] && rm $HOME/temp.txt
-    else
-    __besman_echo_white "No playbook available"
-    fi
 
+    __besman_list_playbooks
+
+elif [[ ( -n $flag ) && ( $flag == "--roles" ) ]]; then
+    if [[ -z "$BESMAN_GH_TOKEN" ]]; then
+        __besman_echo_yellow "Github token missing. Please use the below command to export the token"
+        __besman_echo_no_colour ""
+        __besman_echo_no_colour "$ bes set BESMAN_GH_TOKEN <copied token>"
+        __besman_echo_no_colour ""
+        return 1
+    fi
+    __besman_list_roles
 else
+    __besman_list_envs 
+
+fi
+}
+function __besman_list_envs()
+{
     __besman_check_repo_exist || return 1
     __besman_update_list
     # __besman_echo_no_colour "Github Org    Repo                             Environment     Version"
@@ -61,7 +68,6 @@ else
         __besman_echo_yellow "If you wish to change the repo run the below command"
         __besman_echo_yellow "$ bes set BESMAN_ENV_REPOS <GitHub Org>"
     fi
-fi
 }
 function __besman_check_repo_exist()
 {
@@ -101,7 +107,60 @@ function __besman_update_list()
         __besman_secure_curl "$path" > "$bes_list"
     fi
 
+}
+
+# Function to extract repository names from a JSON response
+function __besman_extract_repo_names()
+{
+  echo "$1" | grep -oP '"full_name": "\K[^"]+'
+}
 
 
+function __besman_list_roles()
+{
+    local api_url repo_names all_repo_names page_num ansible_roles
 
+    api_url="https://api.github.com/orgs/$BESMAN_NAMESPACE/repos?per_page=100&page=1"
+
+    # Get the first page of repository names
+    repo_names=$(curl -s -H "Authorization: token $BESMAN_GH_TOKEN" "$api_url")
+
+    # Extract repository names from the first page
+    all_repo_names=$(__besman_extract_repo_names "$repo_names")
+    page_num=1
+    # Check if there are more pages and continue fetching if needed
+    while [ "$(echo "$repo_names" | grep -c '"full_name"')" -eq 100 ]; do
+        page_num=$((page_num + 1))
+        api_url="https://api.github.com/orgs/$BESMAN_NAMESPACE/repos?per_page=100&page=$page_num"
+        repo_names=$(curl -s -H "Authorization: token $BESMAN_GH_TOKEN" "$api_url")
+        all_repo_names="$all_repo_names
+        $(__besman_extract_repo_names "$repo_names")"
+    done
+
+    ansible_roles=$(echo "$all_repo_names" | grep "ansible-role-*")
+    
+    printf "%-14s %10s \n" "Github Org" "Repo"
+    __besman_echo_no_colour "-----------------------------------"
+    for i in $ansible_roles
+    do
+        converted_i=$(echo "$i" | sed "s|/| |g")
+        read -r org repo <<< "$converted_i"
+        printf "%-14s %-32s \n" "$org" "$repo"
+    done
+    
+
+}
+
+function __besman_list_playbooks()
+{
+    if [[ -d $BESMAN_DIR/playbook  ]]; then
+        [[ -z $(ls $BESMAN_DIR/playbook | grep -v "README.md") ]] && __besman_echo_white "No playbook available" && return 1
+        ls $BESMAN_DIR/playbook >> $HOME/temp.txt
+        __besman_echo_no_colour "Available playbooks"
+        __besman_echo_no_colour "-------------------"
+        cat $HOME/temp.txt | grep -v "README.md"
+        [[ -f $HOME/temp.txt ]] && rm $HOME/temp.txt
+    else
+    __besman_echo_white "No playbook available"
+    fi
 }
