0a7b92d5c413c9d740323bd4272f6047ff3ec816
asa1997 <arunsureshampadath@gmail.com>
2024-03-07 22:03:45 +0530

    Added option to overwrite existing playbook file

--- src/main/bash/commands/besman-run.sh
+++ src/main/bash/commands/besman-run.sh
@@ -1,43 +1,53 @@
 #!/bin/bash
 
-function __bes_run()
-{
+function __bes_run() {
     local playbook_name playbook_version
 
     playbook_name="$1"
     playbook_version="$2"
 
     __besman_fetch_playbook "$playbook_name" "$playbook_version" || return 1
-    
+
     source "$BESMAN_PLAYBOOK_DIR/besman-$playbook_name-$playbook_version-playbook.sh"
-    
+
     __besman_launch
 
     unset playbook_name playbook_version
 
 }
 
-function __besman_fetch_playbook()
-{
-    local playbook_name playbook_version lifecycle_file steps_file lifecyle_file_url steps_file_url
+function __besman_fetch_playbook() {
+    local playbook_name playbook_version lifecycle_file lifecyle_file_url 
 
     playbook_name="$1"
     playbook_version="$2"
 
     lifecycle_file="$BESMAN_PLAYBOOK_DIR/besman-$playbook_name-$playbook_version-playbook.sh"
     lifecyle_file_url="https://raw.githubusercontent.com/$BESMAN_NAMESPACE/$BESMAN_PLAYBOOK_REPO/main/playbooks/besman-$playbook_name-$playbook_version-playbook.sh"
-    
-    # steps_file="$BESMAN_PLAYBOOK_DIR/besman-$playbook_name-$playbook_version-steps.sh"
-    # steps_file_url="https://raw.githubusercontent.com/$BESMAN_NAMESPACE/$BESMAN_PLAYBOOK_REPO/main/playbooks/besman-$playbook_name-$playbook_version-steps.sh"
 
-    __besman_check_url_valid "$lifecyle_file_url" || return 1
-    # __besman_check_url_valid "$steps_file_url" || return 1
-    
-    touch "$lifecycle_file" 
+    if [[ -f $lifecycle_file ]]; then
+        __besman_echo_yellow "Playbook $playbook_name $playbook_version exist."
 
-    __besman_secure_curl "$lifecyle_file_url" >> "$lifecycle_file"
+        read -rp "Do you wish to overwrite(y/n):" overwrite
+        if [[ "$overwrite" == "y" ]]; then
+            rm "$lifecycle_file"
+            __besman_check_url_valid "$lifecyle_file_url" || return 1
 
-    unset playbook_name playbook_version lifecycle_file steps_file lifecyle_file_url steps_file_url
+            touch "$lifecycle_file"
 
-    
+            __besman_secure_curl "$lifecyle_file_url" >>"$lifecycle_file"
+        fi
+
+    else
+        __besman_check_url_valid "$lifecyle_file_url" || return 1
+
+        touch "$lifecycle_file"
+
+        __besman_secure_curl "$lifecyle_file_url" >>"$lifecycle_file"
+
+    fi
+
+
+    unset playbook_name playbook_version lifecycle_file lifecyle_file_url 
+
 }
