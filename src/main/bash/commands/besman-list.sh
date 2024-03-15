c1bcf1e76ae9b6edae16bc452a8647585f4bacf3
asa1997 <arunsureshampadath@gmail.com>
2024-03-15 12:22:56 +0530

    Fixed #104

--- src/main/bash/commands/besman-list.sh
+++ src/main/bash/commands/besman-list.sh
@@ -26,12 +26,18 @@ fi
 }
 function __besman_list_envs()
 {
+    local current_version current_env local_annotation remote_annotation
     __besman_check_repo_exist || return 1
     __besman_update_list
     # __besman_echo_no_colour "Github Org    Repo                             Environment     Version"
     # __besman_echo_no_colour "-----------------------------------------------------------------------------------"
 
+    [[ -f "$BESMAN_DIR/var/current" ]] &&  current_env=$(cat "$BESMAN_DIR/var/current")
+    [[ -f "$BESMAN_DIR/envs/besman-$current_env/current" ]] && current_version=$(cat "$BESMAN_DIR/envs/besman-$current_env/current")
 
+    installed_annotation=$(__besman_echo_red "*")
+    remote_annotation=$(__besman_echo_yellow "^")
+    
     # For listing environments
     printf "%-14s %-32s %-25s %-8s\n" "Github Org" "Repo" "Environment" "Version"
     __besman_echo_no_colour "-----------------------------------------------------------------------------------"
@@ -47,11 +53,23 @@ function __besman_list_envs()
     do 
         converted_line=$(echo "$line" | sed 's|,|/|g')
         read -r org repo env version <<< "$converted_line"
-        printf "%-14s %-32s %-25s %-8s\n" "$org" "$repo" "$env" "$version"     
+        if [[ ("$env" == "$current_env") && ("$version" == "$current_version") ]] 
+        then
+            printf "%-14s %-32s %-25s %-8s\n" "$org" "$repo" "$env" "$version$installed_annotation"
+        else
+            printf "%-14s %-32s %-25s %-8s\n" "$org" "$repo" "$env" "$version$remote_annotation"
+            
+        fi
         
     done < "$BESMAN_DIR/var/list.txt"
     IFS=$OLD_IFS
 
+    __besman_echo_no_colour ""
+
+    __besman_echo_no_colour "==================================================================================="
+    __besman_echo_no_colour "$remote_annotation - remote environment"
+    __besman_echo_no_colour "$installed_annotation - installed environment"
+    __besman_echo_no_colour "==================================================================================="
     __besman_echo_no_colour ""
 
     unset flag arr env list
@@ -151,46 +169,16 @@ function __besman_list_roles()
 
 }
 
-function __besman_get_playbook_details()
-{
-    local scripts_file
-
-    scripts_file="$BESMAN_DIR/scripts/besman-get-playbook-details.py"
-
-    [[ ! -f "$scripts_file" ]] && __besman_echo_red "Could not find $scripts_file" && return 1
-
-    python3 "$scripts_file"
-}
-
 function __besman_list_playbooks()
 {
-
-
-    local playbook_details_file playbook_details
-
-    playbook_details_file="$BESMAN_DIR/tmp/playbook_details.txt"
-
-    __besman_get_playbook_details || return 1
-
-    playbook_details=$(cat "$playbook_details_file")
-
-    [[ ( ! -f "$playbook_details_file" ) || ( -z $playbook_details ) ]] && __besman_echo_red "Could not find playbook details" && return 1
-
-    printf "%-14s %-10s %-15s %-8s\n" "Name" "Version" "Type" "Author"
-    __besman_echo_no_colour "----------------------------------------------------------------------"
-
-    OLD_IFS=$IFS
-    IFS=" "
-      
-    while read -r line; 
-    do 
-        # converted_line=$(echo "$line" | sed 's|,|/|g')
-        read -r name version type author <<< "$line"
-        printf "%-14s %-10s %-15s %-8s\n" "$name" "$version" "$type" "$author"     
-        
-    done < "$playbook_details_file"
-    IFS=$OLD_IFS
-
-    [[ -f $playbook_details_file ]] && rm "$playbook_details_file"
-
+    if [[ -d $BESMAN_DIR/playbook  ]]; then
+        [[ -z $(ls $BESMAN_DIR/playbook | grep -v "README.md") ]] && __besman_echo_white "No playbook available" && return 1
+        ls $BESMAN_DIR/playbook >> $HOME/temp.txt
+        __besman_echo_no_colour "Available playbooks"
+        __besman_echo_no_colour "-------------------"
+        cat $HOME/temp.txt | grep -v "README.md"
+        [[ -f $HOME/temp.txt ]] && rm $HOME/temp.txt
+    else
+    __besman_echo_white "No playbook available"
+    fi
 }
