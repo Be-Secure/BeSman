7bfda192c3ea10eb0e8b7db0ccd69f0a41be2927
asa1997 <arunsureshampadath@gmail.com>
2024-03-18 17:42:31 +0530

    Removed unused code

--- src/main/bash/commands/besman-run.sh
+++ src/main/bash/commands/besman-run.sh
@@ -31,38 +31,3 @@ function __bes_run() {
 
 }
 
-function __besman_fetch_playbook() {
-    local playbook_name playbook_version lifecycle_file lifecyle_file_url 
-
-    playbook_name="$1"
-    playbook_version="$2"
-
-    lifecycle_file="$BESMAN_PLAYBOOK_DIR/besman-$playbook_name-$playbook_version-playbook.sh"
-    lifecyle_file_url="https://raw.githubusercontent.com/$BESMAN_NAMESPACE/$BESMAN_PLAYBOOK_REPO/main/playbooks/besman-$playbook_name-$playbook_version-playbook.sh"
-
-    if [[ -f $lifecycle_file ]]; then
-        __besman_echo_yellow "Playbook $playbook_name $playbook_version exist."
-
-        read -rp "Do you wish to overwrite(y/n):" overwrite
-        if [[ "$overwrite" == "y" ]]; then
-            rm "$lifecycle_file"
-            __besman_check_url_valid "$lifecyle_file_url" || return 1
-
-            touch "$lifecycle_file"
-
-            __besman_secure_curl "$lifecyle_file_url" >>"$lifecycle_file"
-        fi
-
-    else
-        __besman_check_url_valid "$lifecyle_file_url" || return 1
-
-        touch "$lifecycle_file"
-
-        __besman_secure_curl "$lifecyle_file_url" >>"$lifecycle_file"
-
-    fi
-
-
-    unset playbook_name playbook_version lifecycle_file lifecyle_file_url 
-
-}
