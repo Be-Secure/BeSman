7bb801d801a79b6520d1581fc813265a970ab32d
asa1997 <arun.suresh@wipro.com>
2024-03-28 12:25:18 +0530

    Updated

--- src/main/bash/commands/besman-install.sh
+++ src/main/bash/commands/besman-install.sh
@@ -112,7 +112,7 @@ function __besman_get_remote_env {
 	curl_flag=true
 	__besman_check_url_valid "$env_url" || return 1
 	__besman_secure_curl "$env_url" >>"${BESMAN_DIR}/envs/besman-${environment_name}.sh"
-	[[ "$?" -ne 0 ]] && __besman_echo_red "Failed while trying to get the besman-${environment_name}.sh" && return 1
+	# [[ "$?" -ne 0 ]] && __besman_echo_red "Failed while trying to get the besman-${environment_name}.sh" && return 1
 	# config_url="https://raw.githubusercontent.com/${env_repo_namespace}/${env_repo}/master/${ossp}/${version_id}/besman-$environment_name-config.yaml"
 
 	# if [[ ( -n $BESMAN_LIGHT_MODE ) && ( $BESMAN_LIGHT_MODE == "false" ) && ( ! -f "$HOME/besman-${ossp}-${env_type}-env-config.yaml" ) ]]; then
