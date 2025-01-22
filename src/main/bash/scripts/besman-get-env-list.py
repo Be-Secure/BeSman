90bc9148b6ad58a3fba618a60d9da206e1f2e345
asa1997 <arunsureshampadath@gmail.com>
2025-01-22 17:28:42 +0530

    Fixed #228

--- src/main/bash/scripts/besman-get-env-list.py
+++ src/main/bash/scripts/besman-get-env-list.py
@@ -33,7 +33,10 @@ def get_env_list():
                 data = json.load(local_file)
         else:
             # Fetch JSON from URL
-            response = requests.get(url)
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+            }
+            response = requests.get(url, headers=headers)
             response.raise_for_status()  # Raise an exception for bad responses (4xx or 5xx)
             data = response.json()
             # print(data)
